7a7ff840cadeea8b849fae1aa705322997107dad
xxz <xxz911@rambler.ru>
2022-12-24 22:34:43 +0300

    Дополнительный коммит к предыдущему, добавлены настрайки для админки Пользователи, загружены тестовые картинки в БД

--- poyasnizagistu/users/admin.py
+++ poyasnizagistu/users/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.safestring import mark_safe
+
 from .models import CustomUser
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
+    list_display = ('username', 'first_name', 'last_name', 'email', 'get_avatar', 'age', 'gender')
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'age', 'gender')
+    list_filter = ('age', 'gender', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+
+
+
     add_fieldsets = (
         *UserAdmin.add_fieldsets,
         (
             'Info',
            {
-              'fields': ('first_name', 'last_name', 'email', 'avatar', 'age', 'gender')
+              'fields': ('first_name', 'last_name', 'email','get_avatar', 'avatar', 'age', 'gender')
            }
         )
      )
@@ -21,8 +29,13 @@ class CustomUserAdmin(UserAdmin):
         (
             'Info',
             {
-                'fields': ('avatar',)
+                'fields': ('get_avatar', 'avatar')
             }
         )
      )
 
+    readonly_fields = ('get_avatar', 'last_login', 'date_joined')
+    def get_avatar(self, object):
+        return mark_safe(f"<img src='{object.avatar.url}' width=6"
+                             f"0>")
+    get_avatar.short_description = 'Аватар'
