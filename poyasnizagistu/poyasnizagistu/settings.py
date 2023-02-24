494c2f9cdc291d34cebd1f22cf0409aa1caba95e
xxz <xxz911@rambler.ru>
2023-02-24 22:52:47 +0300

    Удалил тестовый пароль в settings

--- poyasnizagistu/poyasnizagistu/settings.py
+++ poyasnizagistu/poyasnizagistu/settings.py
@@ -246,7 +246,7 @@ CAPTCHA_LENGTH = 5
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "hystologyvk@gmail.com"
-EMAIL_HOST_PASSWORD = "kwmiuyebocejmjnd"
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
